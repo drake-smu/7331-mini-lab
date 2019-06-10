a589571ac2729499938336700eb48d5e0f86f915
Landcruiser87 <andyheroy@gmail.com>
2019-06-10 16:16:38 -0500

    Inserting Data understanding markdown section

--- analysis/eda.py
+++ analysis/eda.py
@@ -13,7 +13,7 @@ except:
 
 # ### Carson Drake, Che Cobb, David Josephs, Andy Heroy
 
-## Table of Contents
+# ## Table of Contents
 # We'll have to put one in tomorrow, i'm just going to start writing 
 
 #%% [markdown] 
@@ -23,9 +23,9 @@ except:
 #     ### Section 1a: Describe the purpose of the data set you selected
 
 #%% [markdown] 
-# # Business Understanding
-# ## Section 1a: Describe the purpose of the data set you selected.  
-# We chose this dataset from the UCI's machine learning repository for its
+# ## Section 1: Business Understanding
+# ### Section 1a: Describe the purpose of the data set you selected.  
+# We chose this dataset from the UCI's machine learning repository for its categorical
 # predictive attributes.  It contains 1994 Census data pulled from the US Census
 # database.  The prediction task we've set forth is to predict if a persons
 # salary range is >50k in a 1994, based on the various categorical/numerical
@@ -33,7 +33,7 @@ except:
 #
 # https://archive.ics.uci.edu/ml/datasets/census+income
 #
-# ## Section 1b: Describe how you would define and measure the outcomes from the dataset. 
+# ### Section 1b: Describe how you would define and measure the outcomes from the dataset. 
 # (That is, why is this data important and how do you know if you have mined
 # useful knowledge from the dataset? How would you measure the effectiveness of
 # a good prediction algorithm? Be specific.)
@@ -48,14 +48,33 @@ except:
 #     years of our life should we be working hardest in order to make the most
 #     money. 
 #   * Does where you come from influence your income? (native country)
-#   *
+
+#%% [markdown] 
+# ## Section 2: Data Understanding
+# ### Section 2a: Describe the meaning and type of data for each attribute
+# Here we will dicuss each attribute and give some description about its ranges. 
 
 
+# Category - Description - Range
+# #### Cagtegorical Attributes 
+# * workclass - Which sector do they work in?
+# * education - What level of education recieved?
+# * marital_status - What is their marriage history
+# * occupation - What do they do for a living
+# * relationship - 
+# * race - What is the subjects race
+# * gender - What is the subjects gender
+# * native_country - Where is the subject originally from
+# * income_bracket - Do they make over or under 50k/year
 
-
-
-
-
+# #### Continuous Attributes
+# * age - How old is the subject - 
+# * fnlwgt - Weighting of the observation
+# * education_num - numerical encoding of education variable
+# * capital_gain - 
+# * capital_loss - 
+# * hours_per_week - How many hours a week did they work? 
+# 
 #%%
 #Add library references
 import pandas as pd
